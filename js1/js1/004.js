53ab79cdb5f1902a19367cf39049b942191ad64e
ptp-vertex <ptp.vertex@gmail.com>
2026-07-08 11:10:37 +0700

    Delete js1/js1 directory

--- js1/js1/004.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('click',function(){
- setTimeout(function(){
-   var el=document.getElementById('schoolhub-mini-calendar-backdrop');
-   if(el && el.parentElement!==document.body){
-      document.body.appendChild(el);
-   }
- },50);
-});
--- js1/js1/087_schoolhub-open-user-plan-always-load-fix.js
+++
@@ -1,3 +0,0 @@
-
-/* Disabled: duplicate openUserPlanSelector wrapper removed. Canonical plan selector flow is installed at the end of the file. */
-(function(){ window.__schoolhub_open_user_plan_always_load_fix_disabled = true; })();
